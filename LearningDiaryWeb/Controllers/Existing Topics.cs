94ee631a7dd2198c9c85c65ad35f4ac785b50028
AnnaVat <a.vatjalainen@gmail.com>
2022-07-24 22:28:20 +0300

    first commit

---
+++ LearningDiaryWeb/Controllers/Existing Topics.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningDiaryWeb.Controllers
+{
+    public class Existing_Topics : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LearningDiaryWeb/Data/LearningDiaryWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LearningDiaryWeb.Models;
+
+namespace LearningDiaryWeb.Data
+{
+    public class LearningDiaryWebContext : DbContext
+    {
+        public LearningDiaryWebContext (DbContextOptions<LearningDiaryWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LearningDiaryWeb.Models.Topic> Topic { get; set; }
+    }
+}
